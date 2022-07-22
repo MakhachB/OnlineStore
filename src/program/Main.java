8d99f9e7102040255df18c0762d96002ab7a0653
Makhach <bmaha112002@gmail.com>
2022-07-22 17:49:46 +0300

    creating databases

--- src/program/Main.java
+++ src/program/Main.java
@@ -19,6 +19,7 @@ public class Main {
 
     private static void printGreetings() {
         System.out.println("Welcome to our app!");
+        System.out.println("To exit app print 'exit'");
         System.out.println("To continue answer the question:");
         System.out.println("You're client (1) or employee (2) ?");
     }
